ae8b1f13b625e8b7522dc52aaa40a85586b341f5
isTriGer <138494188+isTriGer@users.noreply.github.com>
2023-08-28 19:18:02 +0900

    Add files via upload

---
+++ 9/ad/app_ad/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from .models import Advertisement
+
+
+# Create your views here.
+
+
+def index(request):
+    advertisements = Advertisement.objects.all()
+    context = {'advertisements': advertisements}
+    return render(request, 'index.html', context)
+
+
+def top_sellers(request):
+    return render(request, 'top-sellers.html')
+
+
+def advertisement(request):
+    return render(request, 'advertisement.html')
+
+
+def advertisement_post(request):
+    return render(request, 'advertisement-post.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def profile(request):
+    return render(request, 'profile.html')
+
+
+def register(request):
+    return render(request, 'register.html')
